e66629c83cb38743ae21821eac455d265e954428
artur31415926 <artur31415926@gmail.com>
2021-11-01 08:07:27 -0300

    art loading

--- Entropy/Game1.cs
+++ Entropy/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Entropy
 {
@@ -8,6 +9,11 @@ namespace Entropy
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Texture2D tankTexture;
+        Point tankPosition = new Point(100, 100);
+        Point tankSize = new Point(50, 50);
+        int tankSpeed = 3;
 
         public Game1()
         {
@@ -20,6 +26,8 @@ namespace Entropy
         {
             // TODO: Add your initialization logic here
 
+            
+
             base.Initialize();
         }
 
@@ -28,6 +36,7 @@ namespace Entropy
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            tankTexture = Content.Load<Texture2D>("Tiles/Tanks/tank1_body");
         }
 
         protected override void Update(GameTime gameTime)
@@ -37,6 +46,20 @@ namespace Entropy
 
             // TODO: Add your update logic here
 
+            Keys[] pressedKeys = Keyboard.GetState().GetPressedKeys();
+
+            for (int i = 0; i < pressedKeys.Length; ++i)
+            {
+                if (pressedKeys[i].Equals(Keys.W))
+                    tankPosition.Y -= tankSpeed;
+                else if (pressedKeys[i].Equals(Keys.S))
+                    tankPosition.Y += tankSpeed;
+                else if (pressedKeys[i].Equals(Keys.A))
+                    tankPosition.X -= tankSpeed;
+                else if (pressedKeys[i].Equals(Keys.D))
+                    tankPosition.X += tankSpeed;
+            }
+
             base.Update(gameTime);
         }
 
@@ -45,6 +68,15 @@ namespace Entropy
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin(SpriteSortMode.Immediate, null, null, null, null, null, null);
+
+            Rectangle tankRect = new Rectangle(tankPosition.X, tankPosition.Y, tankSize.X, tankSize.Y);
+
+            _spriteBatch.Draw(tankTexture, tankPosition, null, Color.White, (float)Math.PI, new Vector2(0, 0), 1, SpriteEffects.None, 0);
+            //_spriteBatch.Draw(tankTexture, new Rectangle(tankPosition.X, tankPosition.Y, tankSize.X, tankSize.Y), Color.White);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
