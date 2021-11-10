951caeeebe72c3d1d362007c1c8b47c38c6f3067
artur31415926 <artur31415926@gmail.com>
2021-11-10 16:02:15 -0300

    New Bullet Class

---
+++ Entropy/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entropy
+{
+    class Bullet
+    {
+
+        Vector2 Position = new Vector2();
+        Vector2 Velocity = new Vector2();
+        public Texture2D Texture;
+        float Orientation = 0f;
+
+        public Bullet()
+        { 
+        }
+
+        public Bullet(Texture2D texture, Vector2 position, Vector2 velocity, float orientation)
+        {
+            Texture = texture;
+            Position = position;
+            Velocity = velocity;
+            Orientation = orientation;
+        }
+
+        public void ApplyCurrentVelocity()
+        {
+
+        }
+    }
+}
--- Entropy/Game1.cs
+++ Entropy/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections;
 
 namespace Entropy
 {
@@ -11,9 +12,25 @@ namespace Entropy
         private SpriteBatch _spriteBatch;
 
         Texture2D tankTexture;
-        Point tankPosition = new Point(100, 100);
+        Vector2 tankPosition = new Vector2(200, 200);
         Point tankSize = new Point(50, 50);
         int tankSpeed = 3;
+
+        float tankOrientation = 0f;
+        float rotationSpeed = (float)Math.PI / 100;
+
+        Boolean IsNewShoot = false;
+
+        ArrayList Bullets = new ArrayList();
+        Texture2D BulletTexture;
+
+        float BulletBaseSpeed = 5f;
+
+        public void ApplySpeed(Vector2 newLinearSpeed, float newAngularSpeed)
+        {
+            tankPosition = tankPosition + newLinearSpeed;
+            tankOrientation += newAngularSpeed;
+        }
 
         public Game1()
         {
@@ -37,6 +54,7 @@ namespace Entropy
 
             // TODO: use this.Content to load your game content here
             tankTexture = Content.Load<Texture2D>("Tiles/Tanks/tank1_body");
+            BulletTexture = Content.Load<Texture2D>("Tiles/Tanks/tank1_gun");
         }
 
         protected override void Update(GameTime gameTime)
@@ -50,14 +68,40 @@ namespace Entropy
 
             for (int i = 0; i < pressedKeys.Length; ++i)
             {
+                Vector2 linearSpeed = new Vector2(0, 0);
+                float angularSpeed = 0f;
+                float dir = 0;
+
+
                 if (pressedKeys[i].Equals(Keys.W))
-                    tankPosition.Y -= tankSpeed;
+                    dir = 1;
                 else if (pressedKeys[i].Equals(Keys.S))
-                    tankPosition.Y += tankSpeed;
-                else if (pressedKeys[i].Equals(Keys.A))
-                    tankPosition.X -= tankSpeed;
-                else if (pressedKeys[i].Equals(Keys.D))
-                    tankPosition.X += tankSpeed;
+                    dir = -1;
+
+                if (pressedKeys[i].Equals(Keys.E))
+                    angularSpeed = rotationSpeed;
+                else if (pressedKeys[i].Equals(Keys.Q))
+                    angularSpeed = - rotationSpeed;
+
+                if (!IsNewShoot && pressedKeys[i].Equals(Keys.Space))
+                {
+                    //then shoot!
+                    IsNewShoot = true;
+
+                    Bullets.Add(new Bullet(BulletTexture, tankPosition, new Vector2(BulletBaseSpeed * (float)Math.Sin(tankOrientation), -BulletBaseSpeed * (float)Math.Cos(tankOrientation)), tankOrientation));
+                }
+
+                ApplySpeed(new Vector2(dir * (float)Math.Sin(tankOrientation), - dir * (float)Math.Cos(tankOrientation)), angularSpeed);
+            }
+
+            if(IsNewShoot)
+            {
+                BulletPosition += BulletVelocity;
+
+                if(BulletPosition.X > GraphicsDevice.Viewport.Width || BulletPosition.X < 0 || BulletPosition.Y > GraphicsDevice.Viewport.Height || BulletPosition.Y < 0)
+                {
+                    IsNewShoot = false;
+                }
             }
 
             base.Update(gameTime);
@@ -71,10 +115,21 @@ namespace Entropy
 
             _spriteBatch.Begin(SpriteSortMode.Immediate, null, null, null, null, null, null);
 
-            Rectangle tankRect = new Rectangle(tankPosition.X, tankPosition.Y, tankSize.X, tankSize.Y);
+            
 
-            _spriteBatch.Draw(tankTexture, tankPosition, null, Color.White, (float)Math.PI, new Vector2(0, 0), 1, SpriteEffects.None, 0);
+            Rectangle tankRect = new Rectangle((int)tankPosition.X, (int)tankPosition.Y, tankSize.X, tankSize.Y);
+
+            _spriteBatch.Draw(tankTexture, tankPosition, null, Color.White, tankOrientation, new Vector2(tankTexture.Width / 2, tankTexture.Height / 2), 1, SpriteEffects.None, 0);
             //_spriteBatch.Draw(tankTexture, new Rectangle(tankPosition.X, tankPosition.Y, tankSize.X, tankSize.Y), Color.White);
+
+            if(Bullets.Count > 0)
+            {
+                for (int i = 0; i < Bullets.Count; ++i)
+                {
+                    _spriteBatch.Draw(Bullets[i].Texture, BulletPosition, null, Color.Red, BulletOrientation, new Vector2(BulletTexture.Width / 2, BulletTexture.Height / 2), 1, SpriteEffects.None, 0);
+                }
+            }
+
 
             _spriteBatch.End();
 
